14b8d3d2d3724cc696b255bb6ea28e2f80232a27
Aidan Elm <elmaidan@gmail.com>
2024-08-02 15:19:40 -0400

    improve code readability

--- scanner.py
+++ scanner.py
@@ -1,54 +1,79 @@
+# Author: Aidan Elm | 2024-08-02
+
+""" Install nmap module via pip as python-nmap. """
+
+import sys # Exit
+
 # Import nmap module
 try:
     import nmap
 except ModuleNotFoundError:
-    print("Error: python-nmap module not found.")
-    input("Press Enter to continue...")
-    exit()
+    print('Error: python-nmap module and/or nmap not found.')
+    input('Press Enter to continue...')
+    sys.exit()
 
 # Ensure nmap is added to PATH
-def initialize_nmap():
+def initialize_nmap() -> nmap.PortScanner:
+
+    """
+    Inputs: None
+    Outputs: PortScanner object
+    """
+
     try:
         return nmap.PortScanner()
     except nmap.nmap.PortScannerError:
-        print("Error: Nmap must be installed and added to PATH.")
-        input("Press Enter to continue...")
-        exit()
+        print('Error: Nmap must be installed and added to PATH.')
+        input('Press Enter to continue...')
+        sys.exit()
 
 # Scan a given host with vulners script
-def scan_ip(nmScan, address):
-    print("Gathering information. This may take some time...\n")
-    nmScan.scan(address, arguments = "--script vulners -sV")
+def scan_ip(nmap_scanner: nmap.PortScanner, address: str) -> None:
+
+    """
+    Inputs: PortScanner object, IP address
+    Outputs: None (modifies PortScanner object)
+    """
+
+    print('Gathering information. This may take some time...\n')
+    nmap_scanner.scan(address, arguments = '--script vulners -sV')
 
     # If nothing shows up (could be for multiple reasons)
-    if not nmScan.all_hosts():
-        print("Error: No information found. Please ensure proper input, ensure vulners script is installed, and try again.")
-        print("It is also possible that all ports are closed.")
-        input("Press Enter to continue...\n")
-        exit()
+    if not nmap_scanner.all_hosts():
+        print('Error: No information found.')
+        print('Please ensure proper input and ensure vulners script is installed.')
+        print('It is also possible that all ports are closed.')
+        input('Press Enter to continue...\n')
+        sys.exit()
 
 # Display vulnerabilities by combing through JSON
-def show_vulnerabilities(nmScan):
-    for host in nmScan.all_hosts():
-        print('Scanning {0} ({1})'.format(host, nmScan[host].hostname()))
-        print('Host status: {0}'.format(nmScan[host].state()))
+def show_vulnerabilities(nmap_scanner: nmap.PortScanner):
 
-        for protocol in nmScan[host].all_protocols():
-            print('\n-------- {0} Protocol --------'.format(protocol.upper()))
+    """
+    Inputs: PortScanner object
+    Outputs: None (prints information)
+    """
 
-            open_ports = nmScan[host][protocol].keys()
+    for host in nmap_scanner.all_hosts():
+        print(f'Scanning {host} ({nmap_scanner[host].hostname()})')
+        print(f'Host status: {nmap_scanner[host].state()}')
+
+        for protocol in nmap_scanner[host].all_protocols():
+            print(f'\n-------- {protocol.upper()} Protocol --------')
+
+            open_ports = nmap_scanner[host][protocol].keys()
             sorted(open_ports)
 
             for port in open_ports:
-                print ('\n-> Open port: {0}'.format(port))
+                print (f'\n-> Open port: {port}')
 
                 # If no vulnerabilities, 'script' may be missing from JSON
-                if 'script' in nmScan[host][protocol][port]:
+                if 'script' in nmap_scanner[host][protocol][port]:
 
                     # Depending on the scan, 'vulners' may be missing from JSON
-                    if 'vulners' in nmScan[host][protocol][port]['script']:
+                    if 'vulners' in nmap_scanner[host][protocol][port]['script']:
                         print('    Vulnerabilities:')
-                        vulns = nmScan[host][protocol][port]['script']['vulners'].split('\n')
+                        vulns = nmap_scanner[host][protocol][port]['script']['vulners'].split('\n')
 
                         for vuln in vulns:
                             if vuln != '':
@@ -57,21 +82,18 @@ def show_vulnerabilities(nmScan):
                                 # Print CVE's
                                 if len(vuln_parts) == 3:
                                     cve, score, link = vuln_parts
-                                    print('    - Identifier: {0}, Score: {1} Link: {2}'.format(cve, score, link))
+                                    print(f'    - Identifier: {cve}, Score: {score} Link: {link}')
 
-# Main function
-def main():
+if __name__ == '__main__':
+
     try:
-        nmScan = initialize_nmap()
-        address = input("Enter the IP/domain to scan: ").replace(" ", "")
+        scanner = initialize_nmap()
+        selected_address = input('Enter the IP/domain to scan: ').replace(' ', '')
 
-        scan_ip(nmScan, address)
-        show_vulnerabilities(nmScan)
+        scan_ip(scanner, selected_address)
+        show_vulnerabilities(scanner)
 
-        input("\nPress Enter to continue...")
+        input('\nPress Enter to continue...')
+
     except KeyboardInterrupt:
-        print("\nScan interrupted by user.")
-
-# Call main only if directly from Python interpreter
-if __name__ == "__main__":
-    main()
+        print('\nScan interrupted by user.')
